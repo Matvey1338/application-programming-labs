b920fbcc10cee4bfcdcc19cbe8df66488cab6910
Matvey1338 <duo163@yandex.ru>
2024-11-10 14:47:22 +0400

    update main and made gitignore

--- lab2/main.py
+++ lab2/main.py
@@ -1,6 +1,6 @@
 import argparse
 from icrawler import download_images, base_dir
-
+from csv import convert_to_csv, csv_file
 
 def _parse_arguments() -> list:
     """
@@ -8,8 +8,19 @@ def _parse_arguments() -> list:
     Returns:
     list: list of the arguments
     """
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+        description="Download target images",
+    )
+    parser.add_argument("keyword", type=str, help="Keyword for image search.")
+    parser.add_argument("-c", "--csv", type=str, default=csv_file, help="Path to csv file.")
+    parser.add_argument("-d", "--dir", type=str, default=base_dir, help="Path to directory where images store.")
+    parser.add_argument("-n", "--number", type=int, default=50, help="Number of downloading files.")
+
+    return parser.parse_args()
 def main():
     args = _parse_arguments()
-
+    images_dir = download_images(args.keyword, args.number, save_dir=args.dir)
+    csv_paths = convert_to_csv(images_dir, csv_filename=args.csv)
 if __name__ == "__main__":
     main()
