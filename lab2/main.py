bafecfeff8db1682fb758dfcc0419d91724e0268
Matvey1338 <duo163@yandex.ru>
2024-11-10 12:24:50 +0400

    Made proto-main

---
+++ lab2/main.py
@@ -0,0 +1,15 @@
+import argparse
+from icrawler import download_images, base_dir
+
+
+def _parse_arguments() -> list:
+    """
+    Parse arguments from stdin in execution moment.
+    Returns:
+    list: list of the arguments
+    """
+def main():
+    args = _parse_arguments()
+
+if __name__ == "__main__":
+    main()
