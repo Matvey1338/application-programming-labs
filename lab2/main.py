c4f4d57bd9d45026f89672b214be8c4ad38e0432
Matvey1338 <duo163@yandex.ru>
2024-11-11 21:33:08 +0400

    minor fixes

--- lab2/main.py
+++ lab2/main.py
@@ -1,6 +1,6 @@
 import argparse
-from icrawler import download_images, base_dir
-from csv import convert_to_csv, csv_file
+from image_to_dir import download_images, base_dir
+from csv_to_file import convert_to_csv, csv_file
 
 def _parse_arguments() -> list:
     """
@@ -18,9 +18,13 @@ def _parse_arguments() -> list:
     parser.add_argument("-n", "--number", type=int, default=50, help="Number of downloading files.")
 
     return parser.parse_args()
+
+
 def main():
     args = _parse_arguments()
     images_dir = download_images(args.keyword, args.number, save_dir=args.dir)
     csv_paths = convert_to_csv(images_dir, csv_filename=args.csv)
+
+
 if __name__ == "__main__":
     main()
