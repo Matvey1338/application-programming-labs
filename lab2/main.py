4db3094b87f82eda4285f99e298217577edbab84
Matvey1338 <duo163@yandex.ru>
2024-11-12 14:00:32 +0400

    update gitignore and main add iterator

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,10 @@
 import argparse
 from argparse import Namespace
+from time import sleep
 
 from image_to_dir import download_images, base_dir
 from csv_to_file import convert_to_csv, csv_file
+from iterator import ImgIterator
 
 
 def _parse_arguments() -> Namespace:
@@ -25,8 +27,18 @@ def _parse_arguments() -> Namespace:
 
 def main():
     args = _parse_arguments()
-    images_dir = download_images(args.keyword, args.number, save_dir=args.dir)
-    csv_paths = convert_to_csv(images_dir, csv_filename=args.csv)
+    try:
+        images_dir = download_images(args.keyword, args.number, save_dir=args.dir)
+        csv_paths = convert_to_csv(images_dir, csv_filename=args.csv)
+    except FileNotFoundError:
+        print(FileNotFoundError)
+        exit()
+
+    image_iterator = ImgIterator(csv_paths)
+    print(f"Rel. and abs. path's for images with \"{args.keyword}\":")
+    for rel_path, abs_path in image_iterator:
+        sleep(0.15)
+        print(rel_path + " " + abs_path)
 
 
 if __name__ == "__main__":
