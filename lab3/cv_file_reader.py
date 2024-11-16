04a273d1d671e6ceb494b95433e4fec572374c3a
Matvey1338 <duo163@yandex.ru>
2024-11-16 14:52:29 +0400

    made print shape of image

--- lab3/cv_file_reader.py
+++ lab3/cv_file_reader.py
@@ -2,6 +2,6 @@ import cv2
 
 
 def image_analyze(path: str):
-    image = cv2.imread(path)
+    image = cv2.imread(path,  cv2.IMREAD_GRAYSCALE)
     height, width = image.shape
     print("Image shape:", width, "x", height)
