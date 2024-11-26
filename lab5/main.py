b2a401ad529bb4c258e6e084cb7ab084060693d9
Matvey1338 <duo163@yandex.ru>
2024-11-26 18:47:10 +0400

    made gui

--- lab5/main.py
+++ lab5/main.py
@@ -1,19 +1,54 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import os
 import sys
+from main_windows_ui import Ui_MainWindow
+from iterator import ImgIterator
 
 
-def application() -> None:
-    app = QApplication(sys.argv)
-    window = QMainWindow()
+class Imagewindow(QtWidgets.QMainWindow):
+    def __init__(self, csv_file):
+        super(Imagewindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-    window.setWindowTitle("Lab 5")
-    window.setGeometry(500,500, 600, 600)
+        self.ui.pushButton.clicked.connect(self.show_next_image)
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(10, 10, 780, 500)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setScaledContents(True)
 
-    window.show()
+        # Итератор для работы с CSV
+        self.image_iterator = iter(ImgIterator(csv_file))
+        self.current_image = None
+
+        self.history = []
+        self.show_next_image()
+
+    def show_next_image(self):
+        """
+        Отображает следующую фотографию из CSV.
+        """
+        try:
+            # Получаем следующий путь к изображению
+            image_path = next(self.image_iterator)[1]
+            self.history.append(image_path)
+            self.display_image(image_path)
+        except StopIteration:
+            self.label.setText("No more images.")
+
+    def display_image(self, image_path):
+        """
+        Загружает и отображает изображение по указанному пути.
+        """
+        if os.path.exists(image_path):
+            pixmap = QPixmap(image_path)
+            self.label.setPixmap(pixmap)
+        else:
+            self.label.setText("Image not found.")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
     sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    application()
