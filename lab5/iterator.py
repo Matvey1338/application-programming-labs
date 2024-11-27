27e60047cf800b6a48a9620bdb175450f71f2f16
Matvey1338 <duo163@yandex.ru>
2024-11-28 01:35:01 +0400

    final program

---
+++ lab5/iterator.py
@@ -0,0 +1,46 @@
+import csv
+import os
+
+
+class ImgIterator:
+    """
+    The iterator class, for read rows from csv
+    """
+
+    def __init__(self, name: str):
+        """
+        Init iterator.
+
+        Parameters:
+        name (str): name of csv-file.
+        """
+        self.csv_file = name
+        self.file_exist = os.path.exists(self.csv_file)
+
+    def __iter__(self):
+        """
+        Iter method.
+        If target file don't exist set self.file_exist as False.
+        """
+
+        if not self.file_exist:
+            return iter([])
+
+        self.file = open(self.csv_file, newline="")
+        self.csvreader = csv.reader(self.file)
+        return self
+
+    def __next__(self):
+        """
+        Next method for iterations.
+        If self.file_exist is not True, don't start the iteration.
+        """
+
+        if not self.file_exist:
+            raise StopIteration
+
+        try:
+            return next(self.csvreader)
+        except StopIteration:
+            self.file.close()
+            raise StopIteration
--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
 import os
 import sys
 from main_windows_ui import Ui_MainWindow
@@ -8,22 +7,30 @@ from iterator import ImgIterator
 
 
 class Imagewindow(QtWidgets.QMainWindow):
-    def __init__(self, csv_file):
+    def __init__(self):
         super(Imagewindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton.clicked.connect(self.show_next_image)
-        self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(10, 10, 780, 500)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setScaledContents(True)
-
-        # Итератор для работы с CSV
-        self.image_iterator = iter(ImgIterator(csv_file))
-        self.current_image = None
-
+        self.ui.btn_csv.clicked.connect(self.choose_csv)
+        self.ui.btn_csv_2.clicked.connect(self.choose_csv)
+        self.ui.btn_right.clicked.connect(self.show_next_image)
+        self.ui.btn_left.clicked.connect(self.show_previous_image)
+        
+        # Переменные для итерации
+        self.image_iterator = None
         self.history = []
+
+    def choose_csv(self):
+        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select Annotation File", "", "CSV Files (*.csv)")
+        if file_path:
+            self.image_iterator = iter(ImgIterator(file_path))
+            self.ui.stackedWidget.setCurrentIndex(1)
+            self.history = []  # Очистить историю
+            self.skip_csv_intro()
+
+    def skip_csv_intro(self):
+        next(self.image_iterator, None)
         self.show_next_image()
 
     def show_next_image(self):
@@ -36,7 +43,35 @@ class Imagewindow(QtWidgets.QMainWindow):
             self.history.append(image_path)
             self.display_image(image_path)
         except StopIteration:
-            self.label.setText("No more images.")
+            self.ui.label.setText("No more images.")
+            self.history.append("decoy")
+            self.ui.btn_right.setEnabled(False)
+
+    def show_previous_image(self):
+        """
+        Отображает предыдущую фотографию, если есть в истории.
+        """
+        if len(self.history) > 1:
+            # Enable the 'Next' button if it was disabled
+            self.history.pop()  # Убираем текущую
+            image_path = self.history[-1]
+            self.display_image(image_path)
+
+            self.ui.btn_right.setEnabled(True)
+            self.reset_iterator_to_current()
+        else:
+            pass
+
+    def reset_iterator_to_current(self) -> None:
+        """
+        Resets the image iterator to start from the current position in the history.
+        """
+        if self.image_iterator:
+            # Recreate the iterator and skip to the current position in history
+            current_position = len(self.history)
+            self.image_iterator = iter(ImgIterator(self.image_iterator.csv_file))
+            for _ in range(current_position + 1):
+                next(self.image_iterator)
 
     def display_image(self, image_path):
         """
@@ -44,11 +79,13 @@ class Imagewindow(QtWidgets.QMainWindow):
         """
         if os.path.exists(image_path):
             pixmap = QPixmap(image_path)
-            self.label.setPixmap(pixmap)
+            self.ui.label.setPixmap(pixmap)
         else:
-            self.label.setText("Image not found.")
+            self.ui.label.setText("Image not found.")
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
+    image = Imagewindow()
+    image.show()
     sys.exit(app.exec_())
